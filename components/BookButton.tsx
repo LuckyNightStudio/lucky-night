878745183b99bb96e5b8fe3effbdf7b98a778a24
LuckyNightStudio <93833201+LuckyNightStudio@users.noreply.github.com>
2021-12-29 22:04:51 +0000

    Add puppa fresh page

--- components/BookButton.tsx
+++ components/BookButton.tsx
@@ -1,4 +1,5 @@
-import {Button} from "@mui/material";
+import {Button, Modal, Box, useTheme, useMediaQuery} from "@mui/material";
+import {useState} from "react";
 
 interface Props {
     color?: string
@@ -7,9 +8,23 @@ interface Props {
 export const ButtonStyle = { borderRadius: 10, border: 'solid 4px black', px: 4}
 
 export const BookButton = ({color = 'primary'}: Props) => {
+    const theme = useTheme();
+    const largerThanMD = useMediaQuery(theme.breakpoints.up('md'));
+    const [isModalOpen, setIsModalOpen] = useState(false)
     return (
-        <Button variant='contained' color={color as any} sx={ButtonStyle}>
-            BOOK A FREE CONSULTATION
-        </Button>
+        <>
+            <Button variant='contained' color={color as any} sx={ButtonStyle} onClick={() => setIsModalOpen(true)}>
+                BOOK A FREE CONSULTATION
+            </Button>
+            <Modal
+                open={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+            >
+                <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: largerThanMD ? 895 : 300, minHeight: 800, overflow: 'auto' }} className='widget'>
+                    <Box onClick={() => setIsModalOpen(false)}>X</Box>
+                    <iframe src="https://koalendar.com/e/free-consultation-15?embed=true" width="100%" height="800px" frameBorder="0"></iframe>
+                </Box>
+            </Modal>
+        </>
     )
 }
---
+++ pages/our-work/puppa-fresh.tsx
@@ -0,0 +1,215 @@
+import type { NextPage } from 'next'
+import {Container, Box, Typography, useMediaQuery} from "@mui/material";
+import image1 from "../../public/puppa-fresh/puppa-fresh-logo.jpg";
+import image2 from "../../public/puppa-fresh/brand-guide-mockup.jpg";
+import image3 from "../../public/puppa-fresh/paper-bag.jpg";
+import image4 from "../../public/puppa-fresh/shampoo-bottle.jpg";
+import image5 from "../../public/puppa-fresh/desktop-site-mockup.jpg";
+import image6 from "../../public/puppa-fresh/web-pages-mockup.jpg";
+import image7 from "../../public/puppa-fresh/quote.jpg";
+import image8 from "../../public/puppa-fresh/logo.jpg";
+import image9 from "../../public/puppa-fresh/home.jpg";
+import image10 from "../../public/puppa-fresh/services.jpg";
+import image11 from "../../public/puppa-fresh/book an appointment.jpg";
+import Masonry from '@mui/lab/Masonry';
+import Image from "next/image";
+import * as React from "react";
+import {underlineStyle} from "../../components/homepage/Section2";
+import {theme} from "../../utils/theme";
+import {BookButton} from "../../components/BookButton";
+import step2 from "../../public/step2.png";
+import {DownloadSection} from "../../components/homepage/DownloadSection";
+
+const PuppaFresh: NextPage = () => {
+    const largerThanSM = useMediaQuery(theme.breakpoints.up('sm'));
+
+    return (
+        <>
+        <Container>
+            <Box sx={{ display: 'flex', flexDirection: largerThanSM ? 'row': 'column' }} pt={8}>
+                <Box sx={{flex: 1, px: 2, mb: 4, alignSelf: 'center'}}>
+                    <Image
+                        src={image1.src}
+                        alt="Puppa fresh logo"
+                        width={image1.width}
+                        height={image1.height}
+                    />
+                </Box>
+                <Box sx={{flex: 1, px: 2, display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}} >
+                    <Box>
+                        <Typography variant='h3'>Puppa Fresh</Typography>
+                        <Typography variant='h5' mb={2}>
+                            <span style={{ display: 'inline-block' }}>
+                                Strategy - Branding - Web Design
+                                <span style={{...underlineStyle, background: theme.palette.greenLight.main}}/>
+                            </span>
+                        </Typography>
+                    </Box>
+                    <Typography fontWeight={600}  mb={2}>
+                        Puppa Fresh is moving on up. From homegrown mobile dog groomer to bricks & mortar with a lifestyle presence, the team appreciate the importance of their positioning in the market.
+                    </Typography>
+                    <Typography  mb={2}>
+                        With the new look and brand presence tied into the upmarket new location, the goal of the brand & web design process was to position Puppa Fresh as a high-end must-visit for the most devoted dog mamas.
+                    </Typography>
+                    <Typography  mb={2}>
+                        Looking to strengthen your position in the market with custom brand & website design?
+                    </Typography>
+                    <Box mb={6}>
+                        <BookButton color='secondary'/>
+                    </Box>
+                </Box>
+            </Box>
+            <Box sx={{display: 'flex'}}>
+                <Box sx={{flex: 1, m: 2}}>
+                    <Image
+                        src={image2.src}
+                        alt="Puppa fresh brand guide mockup"
+
+                        width={image2.width}
+                        height={image2.height}
+                    />
+                </Box>
+                <Box sx={{flex: 1, m: 2}}>
+                    <Image
+                        src={image3.src}
+                        alt="Puppa fresh Brown Bag"
+                        width={image3.width}
+                        height={image3.height}
+                    />
+                </Box>
+            </Box>
+            <Box sx={{ display: 'flex', flexDirection: largerThanSM ? 'row': 'column', alignItems: 'center' }} py={4}>
+                    <Box sx={{flex: 1, px: 2}} >
+                        <Typography fontWeight={600} my={2}>
+                            We worked with Puppa Fresh on Brand Strategy sessions to understand all about their values and goals, and ensure all visuals and messaging were targeted at their dream customer. Once we had clear, defined customer personas we started work on their visual branding.
+                        </Typography>
+                        <Typography  mb={2}>
+                            This included
+                        </Typography>
+                        <ul>
+                            <Typography component='li'>Main logo in full suite of colours</Typography>
+                            <Typography component='li'>2 secondary logos in range of brand colours</Typography>
+                            <Typography component='li'>Badges and icons</Typography>
+                            <Typography component='li'>3 different brand patterns</Typography>
+                            <Typography component='li'>Brand & Strategy guide showing how to use assets appropriately & create new branded content that sells to the right audience</Typography>
+                            <Typography component='li'>Packaging design for dog grooming products</Typography>
+                            <Typography component='li'>Social media templates</Typography>
+                            <Typography component='li'>Shopfront and promotional signage</Typography>
+                        </ul>
+                    </Box>
+                    <Box sx={{flex: 1, px: 2, mb: 4, alignSelf: 'center'}}>
+                        <Image
+                            src={image4.src}
+                            alt="Puppa fresh on shampoo"
+                            width={image4.width}
+                            height={image4.height}
+                        />
+                    </Box>
+                </Box>
+            <Box sx={{display: 'flex', alignItems: 'center', flexDirection: largerThanSM ? 'row': 'column'}}>
+                <Box sx={{flex: 1, m: 2}}>
+                    <Image
+                        src={image5.src}
+                        alt="Puppa fresh website"
+                        width={image5.width}
+                        height={image5.height}
+                    />
+                </Box>
+                <Box sx={{flex: 1, m: 2}}>
+                    <Typography fontWeight={600} mb={2}>
+                        Once visual branding was signed off (with no amendments thanks to our rigorous initial brand strategy sessions) we got to work on creating their custom website - no bulky template sites here.
+                    </Typography>
+                    <Typography mb={2}>
+                        Puppa Fresh’s key goal of their online presence was to encourage people to book appointments, so we had to ensure the booking process was easy to find and simple to complete. We placed plenty of clear calls to action and created a custom pop up booking modal that ensured users never lost their place on the site and could come and go as they needed to. The modal allowed bookings to be completed in under 4 clicks, ensuring a smooth and welcoming process that would encourage online bookings.
+                    </Typography>
+                    <Typography mb={2}>
+                        We worked hard on SEO and trained them in beneficial content creation that would boost their rankings and site authority. We laid the foundations for a front-page site with image optimisation, well-researched keywords and descriptions and page markup, and we know with consistent content creation and growth in popularity, Puppa Fresh’s ranking will continue to improve.
+                    </Typography>
+                </Box>
+            </Box>
+            <Box sx={{display: 'flex'}}>
+                <Box sx={{flex: 1, m: 2}}>
+                    <Image
+                        src={image6.src}
+                        alt="Puppa fresh website mockup"
+
+                        width={image6.width}
+                        height={image6.height}
+                    />
+                </Box>
+                <Box sx={{flex: 1, m: 2}}>
+                    <Image
+                        src={image7.src}
+                        alt="Puppa fresh Quote"
+                        width={image7.width}
+                        height={image7.height}
+                    />
+                </Box>
+            </Box>
+            <Box sx={{ maxWidth: 'sm', margin: 'auto', my: 6 }}>
+                <Box width={50} margin='auto' pb={2}>
+                    <Image
+                        src={step2.src}
+                        alt="Step 2"
+                        width={step2.width}
+                        height={step2.height}
+                    />
+                </Box>
+                <Typography textAlign='center'>
+                    Want to work with us on a custom brand &/or website design that will captivate & convert your dream audience?
+                </Typography>
+                <Box sx={{ my: 4, textAlign: 'center'}}>
+                    <BookButton color='secondary' />
+                </Box>
+            </Box>
+            <Box m={4}>
+                <Image
+                    src={image8.src}
+                    alt="Puppa Fresh logo"
+                    width={image8.width}
+                    height={image8.height}
+                />
+            </Box>
+            <Box m={2}>
+                <Masonry columns={2} spacing={2}>
+                    <Box sx={{ width: '50%'}}>
+                        <Image
+                            src={image9.src}
+                            alt="Puppa Fresh logo"
+                            width={image9.width}
+                            height={image9.height}
+                        />
+                    </Box>
+                    <Box sx={{ width: '50%'}}>
+                        <Image
+                            src={image10.src}
+                            alt="Puppa Fresh logo"
+                            width={image10.width}
+                            height={image10.height}
+                        />
+                    </Box>
+                    <Box sx={{ width: '50%'}}>
+                        <Image
+                            src={image11.src}
+                            alt="Puppa Fresh logo"
+                            width={image11.width}
+                            height={image11.height}
+                        />
+                    </Box>
+                </Masonry>
+            </Box>
+            <Box sx={{ maxWidth: 'sm', margin: 'auto', my: 8 }}>
+                <Typography textAlign='center'>
+                    Want to work with us on a custom brand &/or website design that will captivate & convert your dream audience?
+                </Typography>
+                <Box sx={{ my: 4, textAlign: 'center'}}>
+                    <BookButton color='secondary' />
+                </Box>
+            </Box>
+        </Container>
+    <DownloadSection />
+    </>
+)
+}
+
+export default PuppaFresh
