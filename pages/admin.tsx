2820b821b8564b5d4f605e9acc6ec8020efe8827
LuckyNightStudio <93833201+LuckyNightStudio@users.noreply.github.com>
2022-01-13 20:19:18 +0000

    Ignore ts errors from ntelify

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,9 +1,10 @@
 import dynamic from 'next/dynamic';
 import config from '../cms/config';
 
-const CMS = dynamic(
-    () =>
+// @ts-ignore
+const CMS = dynamic(() =>
         import('netlify-cms-app').then((cms) => {
+            // @ts-ignore
             cms.init({ config });
         }),
     { ssr: false, loading: () => <p>Loading...</p> }
