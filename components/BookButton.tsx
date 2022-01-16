78f92498fc361f31de670cb631a7aa7a4d6533c4
LuckyNightStudio <93833201+LuckyNightStudio@users.noreply.github.com>
2022-01-16 06:44:48 +0000

    Add quick links page

--- components/BookButton.tsx
+++ components/BookButton.tsx
@@ -1,19 +1,22 @@
-import {Button, Modal, Box, useTheme, useMediaQuery} from "@mui/material";
+import {Button} from "@mui/material";
 import {useState} from "react";
 import {BookingModal} from "./BookModal";
+import {SxProps} from "@mui/system";
+import {Theme} from "@mui/material/styles";
 
 interface Props {
     color?: string
+    styles?:  SxProps<Theme>
 }
 
 export const ButtonStyle = { borderRadius: 10, border: 'solid 4px black', px: 4}
 
-export const BookButton = ({color = 'primary'}: Props) => {
+export const BookButton = ({color = 'primary', styles= {}}: Props) => {
     const [isModalOpen, setIsModalOpen] = useState(false)
 
     return (
         <>
-            <Button variant='contained' color={color as any} sx={ButtonStyle} onClick={() => setIsModalOpen(true)}>
+            <Button variant='contained' color={color as any} sx={{...ButtonStyle, ...styles}} onClick={() => setIsModalOpen(true)}>
                 BOOK A FREE CONSULTATION
             </Button>
             <BookingModal isOpen={isModalOpen} setIsOpen={setIsModalOpen}/>
---
+++ pages/quick-links.tsx
@@ -0,0 +1,73 @@
+import {NextPage} from "next";
+import {NextSeo} from "next-seo";
+import {Box, Button, Container, Typography} from "@mui/material";
+import {BookButton, ButtonStyle} from "../components/BookButton";
+import Link from "next/link";
+import * as React from "react";
+import louiseAndNaomi from "../public/louiseAndNaomi.webp";
+import Image from "next/image";
+import {SxProps} from "@mui/system";
+import {Theme} from "@mui/material/styles";
+
+const ButtonStyles: SxProps<Theme> = {...ButtonStyle, width: '100%', mx: 'auto', my: 1, p: 2, textAlign: 'center'}
+
+const Page: NextPage = () => {
+    return (
+        <>
+            <NextSeo
+                title="Quick Links for Lucky Night Studio"
+                description="Strategy-led branding & web design for playful young businesses with immense ambitions"
+            />
+            <Container>
+                <Box maxWidth='sm' py={2} px={2} mx='auto' my={5}>
+                    <Box >
+                        <BookButton color='purple' styles={{width: '100%', mx: 'auto', my: 1, p: 2}}/>
+                        <a href='https://mailchi.mp/f2ca6a15298b/freeseochecklist-luckynightstudio' target='_blank' rel="noreferrer">
+                            <Button variant='contained' sx={ButtonStyles} color={'yellow' as any}>
+                                Free SEO Checklist
+                            </Button>
+                        </a>
+
+                        <Link href='/blog' passHref>
+                            <Button variant='contained' sx={ButtonStyles} color={'greenLight' as any}>
+                                Check out our Blog
+                            </Button>
+                        </Link>
+
+                        <Link href='/our-work' passHref>
+                            <Button variant='contained' sx={ButtonStyles} color='secondary'>
+                                View our Portfolio
+                            </Button>
+                        </Link>
+                    </Box>
+                    <Box sx={{ maxWidth: 300, mx: 'auto', my: 6 }}>
+                        <Link href='/' passHref>
+                            <Image
+                                src={louiseAndNaomi.src}
+                                alt="Picture of Louise and Naomi"
+                                width={louiseAndNaomi.width}
+                                height={louiseAndNaomi.height}
+                            />
+                        </Link>
+                    </Box>
+                    <Typography mb={2} >
+                        <strong>Lucky Night Studio</strong> takes visionary young businesses from overwhelm to laser-focus! Our thoughtful brand identity and web design solutions deeply understand your dream customer base and convert them into a paying reality.
+                    </Typography>
+                    <Typography mb={4}>
+                        <strong>No tricks, no jargon, no bullshit.</strong> Just expert, strategy-led brand identity and websites that build deep emotional connections & convert the right buyers while simplifying your marketing efforts.
+                    </Typography>
+
+                    <Link href='/services' passHref>
+                        <Button variant='contained' sx={ButtonStyles} color='secondary'>
+                            How to work with us
+                        </Button>
+                    </Link>
+                </Box>
+            </Container>
+        </>
+    )
+}
+
+
+
+export default Page
